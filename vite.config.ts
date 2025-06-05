6ec0bdfb7dd9821b77b01f68a4312765c1ef2268
KARTIKEYA NANDIGAMA <116968353+nandigamakarti@users.noreply.github.com>
2025-06-06 01:57:59 +0530

    fix: allow render host in vite preview config

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: "::",
+    host: true,
     port: 8080,
   },
   preview: {
+    host: true,
     port: 10000,
-    allowedHosts: ['habitvault-ucxj.onrender.com'], 
+    allowedHosts: ['all'],
   },
   plugins: [react()],
   resolve: {
